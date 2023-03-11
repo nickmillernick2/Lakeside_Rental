4f1719043799c5e62fbff4f9bcb4b6d642c623e4
nickmillernick2 <121780263+nickmillernick2@users.noreply.github.com>
2023-03-11 10:37:26 -0500

    whole bunch of changes

---
+++ mongodb-mongoose/indexDev.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose' 
+import Query from 'mysql/lib/protocol/sequences/Query.js';
+import Reservations from './model/ReservationDev.js';
+
+
+mongoose.connect("mongodb+srv://nickmiller7875:gGdckHV9mNz8aUDH@cluster0.gaxwh6s.mongodb.net/Lakeside?retryWrites=true&w=majority") 
+
+/// Create a new blog post object
+const res = new Reservations({
+	name: 'Nick Miller Dev',
+	email: 'nickmiller.7875@gmail.com',
+	valid: true,
+	phone: '3306055901',
+	arrive: '2023-01-07T05:00:00.000+00:00',
+	depart: '2023-01-07T05:00:00.000+00:00',
+	message: 'second message',
+	subject: 'second subject'
+  });
+  
+// Insert the article in our MongoDB database
+await res.save();
+
+
+const devRevs = await Reservations.where("name").equals("Nick Miller Dev");
+console.log(devRevs);
+
