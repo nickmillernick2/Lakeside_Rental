f0db5ffc2a57c0c759a91ea2f05fac385e53f888
nickmillernick2 <121780263+nickmillernick2@users.noreply.github.com>
2023-05-06 15:31:14 -0400

    f

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app = express()
+import express from 'express';
 
-app.use(express.static('public'))
+const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
-})
+  res.send('Hello, world!');
+});
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000')
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
