62322ea4b0e8b33c0ab60cd8e2b04b847e6146be
nickmillernick2 <121780263+nickmillernick2@users.noreply.github.com>
2023-05-06 16:13:48 -0400

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,8 +1,12 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const publicPath = path.join(__dirname, 'public');
+
 
 app.use(express.static(publicPath));
 
