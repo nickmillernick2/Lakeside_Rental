a5f9453991360f9bae090d1762fe897ed0f6dd74
nickmillernick2 <121780263+nickmillernick2@users.noreply.github.com>
2023-05-08 20:41:24 -0400

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -26,6 +26,8 @@ import mailgun from 'mailgun-js'
 const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN})
 
 app.post('/send-email', (req, res) => {
+  console.log(req.body.customerEmail)
+
   const {customerEmail, customerPhone, userMessageSubject, 
     userMessage, arrivalDate, departureDate } = req.body
 
